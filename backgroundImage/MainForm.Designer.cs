c47c1dcd0fcafde7c6f01f4229693175779e4e1a
Tom Gregor <tgregor@ivsoftware.com>
2022-07-16 15:05:57 -0600

    Add project files.

---
+++ backgroundImage/MainForm.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace backgroundImage
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRandom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonRandom
+            // 
+            this.buttonRandom.Location = new System.Drawing.Point(37, 86);
+            this.buttonRandom.Name = "buttonRandom";
+            this.buttonRandom.Size = new System.Drawing.Size(112, 34);
+            this.buttonRandom.TabIndex = 1;
+            this.buttonRandom.Text = "Random";
+            this.buttonRandom.UseVisualStyleBackColor = true;
+            this.buttonRandom.Click += new System.EventHandler(this.buttonRandom_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.buttonRandom);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRandom;
+    }
+}
+
---
+++ backgroundImage/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace backgroundImage
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void buttonRandom_Click(object sender, EventArgs e)
+        {
+            // Get the names of the embedded resources in the Images folder
+            var images =
+                this.GetType().Assembly
+                .GetManifestResourceNames()
+                .Where(name => name.Contains(".Images."))
+                .ToArray();
+
+            // Get the random index
+            var index = Rando.Next(images.Length);
+            BeginInvoke((MethodInvoker)delegate 
+            {
+                var raw = images[index];
+                // Parse what you want to go in the Textbox name
+                textBox1.Text = Path.GetFileNameWithoutExtension(raw).Split('.').Last();
+
+                // Load the resource into an image and set it as background
+                using(var stream = this.GetType().Assembly.GetManifestResourceStream(raw))
+                {
+                    this.BackgroundImage = Image.FromStream(stream);
+                }
+            });
+        }
+        Random Rando = new Random(); 
+    }
+}
